3de7d598cacc7f013dca2c87c9d10c632983e5de
Jorgé Reyniers <jorgereyniers@ptr-du4keq0914aoif018w8.18120a2.ip6.access.telenet.be>
2019-07-29 17:59:15 +0200

    Initialize project

---
+++ Crammerock-ios-app/Crammerock-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Crammerock-ios-app
+//
+//  Created by Jorgé Reyniers on 29/07/2019.
+//  Copyright © 2019 Jorgé Reyniers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
