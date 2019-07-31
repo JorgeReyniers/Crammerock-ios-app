d2d40092ed63330979b61416bf3ced9e44c40433
Jorgé Reyniers <jorgereyniers@ptr-du4kepzt6z7n2so2sk2.18120a2.ip6.access.telenet.be>
2019-07-31 21:22:21 +0200

    Added class for each view controller

---
+++ Crammerock-ios-app/Crammerock-ios-app/AdminViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminViewController.swift
+//  Crammerock-ios-app
+//
+//  Created by Jorgé Reyniers on 31/07/2019.
+//  Copyright © 2019 Jorgé Reyniers. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
