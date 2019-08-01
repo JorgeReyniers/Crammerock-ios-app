d9a579405489027728fc231ae8f2d1e123e54470
Jorgé Reyniers <jorgereyniers@ptr-du4kepx92n677shyvxf.18120a2.ip6.access.telenet.be>
2019-08-01 15:18:12 +0200

    added view controller and class for practical page

---
+++ Crammerock-ios-app/Crammerock-ios-app/PracticalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PracticalViewController.swift
+//  Crammerock-ios-app
+//
+//  Created by Jorgé Reyniers on 01/08/2019.
+//  Copyright © 2019 Jorgé Reyniers. All rights reserved.
+//
+
+import UIKit
+
+class PracticalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
