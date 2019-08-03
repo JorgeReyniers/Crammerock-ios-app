76bc179e50196a5eb5eec288b8cc88de5f08b9d5
Jorgé Reyniers <jorgereyniers@ptr-du4kepx92n677shyvxf.18120a2.ip6.access.telenet.be>
2019-08-03 16:41:16 +0200

    created Table View showing all artists

---
+++ Crammerock-ios-app/Crammerock-ios-app/Models/Artist.swift
@@ -0,0 +1,31 @@
+//
+//  Artist.swift
+//  Crammerock-ios-app
+//
+//  Created by Jorgé Reyniers on 03/08/2019.
+//  Copyright © 2019 Jorgé Reyniers. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    var name: String
+    var stage: Stage
+    var dayOfPerformance: DayOfPerformance
+    var startTimeOfPerformance: String
+    
+    init(name: String, stage: Stage, dayOfPerformance: DayOfPerformance, startTimeOfPerformance: String) {
+        self.name = name
+        self.stage = stage
+        self.dayOfPerformance = dayOfPerformance
+        self.startTimeOfPerformance = startTimeOfPerformance
+    }
+}
+
+enum DayOfPerformance {
+    case Vrijdag, Zaterdag
+}
+
+enum Stage: String {
+    case MainNorth = "Main North", MainSouth = "Main South", Club = "Club"
+}
