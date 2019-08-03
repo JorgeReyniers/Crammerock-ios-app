1c493fd42c35bd6881f0a6d1408b9fe02f633c8b
Jorgé Reyniers <jorgereyniers@ptr-du4kepx92n677shyvxf.18120a2.ip6.access.telenet.be>
2019-08-03 21:40:50 +0200

    user can delete artists

--- Crammerock-ios-app/Crammerock-ios-app/ViewControllers/AdminTableViewController.swift
+++ Crammerock-ios-app/Crammerock-ios-app/ViewControllers/AdminTableViewController.swift
@@ -89,31 +89,22 @@ class AdminTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: propertyKeys.manageArtistCell, for: indexPath)
         let artist = artists[indexPath.row]
-        // Configure the cell...
+        // Configure the cell
         cell.textLabel?.text = artist.name
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
     }
-    */
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            artists.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }  
     }
-    */
 
     /*
     // Override to support rearranging the table view.
