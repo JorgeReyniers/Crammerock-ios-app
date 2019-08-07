725a8b25ee4c410dc3d2a08875f0341f412b59a2
Jorgé Reyniers <jorgereyniers@ptr-du4kepx92n677shyvxf.18120a2.ip6.access.telenet.be>
2019-08-07 16:55:11 +0200

    Logo on homepage fades in first time you open the app

--- Crammerock-ios-app/Crammerock-ios-app/ViewControllers/HomeViewController.swift
+++ Crammerock-ios-app/Crammerock-ios-app/ViewControllers/HomeViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var logoHeaderImage: UIImageView!
+    var viewIsAlreadyShown: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if !viewIsAlreadyShown {
+            fadeInLogoHeaderImage()
+            viewIsAlreadyShown = true
+        }
+    }
+    
+    func fadeInLogoHeaderImage() {
+        logoHeaderImage.alpha = 0
+        UIView.animate(withDuration: 2.0, animations: {
+            self.logoHeaderImage.alpha = 1
+        })
+    }
 }
 
